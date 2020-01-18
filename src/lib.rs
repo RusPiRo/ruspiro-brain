82546b8b0288abcdde149f93bd9c57044013cafe
Borrmann <43264484+2ndTaleStudio@users.noreply.github.com>
2020-01-18 17:29:11 +0100

    adjust docu

--- src/lib.rs
+++ src/lib.rs
@@ -9,17 +9,18 @@
 #![feature(associated_type_bounds, asm)]
 
 //! # RusPiRo Brain
-//! This is the **brain** of any RusPiRo - Robot running in an async processing mode. As for a human
-//! it's quite unlikely that everything get's processed in a sequential order. There are always several
-//! tasks that are done in parallel or simultaneous to accomplish a certain goal.
+//! This is the **brain** of any RusPiRo - Robot running in a kind of 'async' processing mode.
+//! As for a human brain it's quite unlikely that everything get's processed in a sequential order.
+//! There are always several thoughts that are processed in parallel/simultaneous to accomplish a
+//! certain goal - come to a conclusion or trigger an activity/movement.
 //!
 //! ## Hint
 //! Rust provides the ``async``/``await`` pattern based on ``Future``s to enable a simple to use
-//! programming style that results in parallel/simultaneous execution. The center of this pattern is
+//! programming style that results in asynchronous execution. The center of this pattern is
 //! the so called ``Executor`` that will drive any open ``Future`` to it's completion. However, the
 //! framework provided by Rust is stabilized but not available for **no_std** crates or **embedded**
-//! developments. Therefore this crate *borrows* the concepts of Rust standard and implement a light-
-//! weight version of this.
+//! developments. Therefore this crate *borrows* the concepts of the Rust standard and implement a
+//! light-weight and feature reduced version of this.
 //!
 //! ## Parts of this crate
 //!
@@ -31,17 +32,20 @@
 //! The names for structures and traits are choosen to give the whole implementation a bit more the
 //! touch of an roboter that will be built with it...
 //!
-//! Trait / Structure | Rust 'std' comparable | Description
+//! Trait / Structure | Rust 'std' comparable | Brief description
 //! ------------------|-----------------------|---------------
-//! ``Thought``       | [``Future``](https://docs.rs/futures/0.3.1/futures/prelude/trait.Future.html) | The abstract thing a brain can process. But to come to a conclusion the ``Brain`` need to actively **think** on it.
-//! ``Task``          | | This a logical unit containg a ``Thought`` the ``Brain`` can think on to drive it to a ``Conclusion``. It also contains a reference to the ``Brain``s ``Spawner`` to be able to put this ``Task`` back onto the Queue of things to think on again.
-//! ``spawn``         | | This function is used to spawn new ``Thought``s to the ``Brain`` so it could think on those to make progress and try to come to a conclusion. The ``Thought`` is internally wrapped into a ``Task`` and pushed to the *need-to-think-on-queue*
+//! [``Thinkeable``]  | [``Future``](https://docs.rs/futures/0.3.1/futures/prelude/trait.Future.html) | The abstract things a brain can process need to implement this trait. But to come to a ``Conclusion`` the ``Brain`` need to actively **think** on it.
+//! [``Thought``]     | | This a logical unit containg a ``Thought`` the ``Brain`` can think on to drive it to a ``Conclusion``. It also contains a reference to the ``Brain``s ``Spawner`` to be able to put this ``Task`` back onto the Queue of things to think on again.
+//! [``Conclusion``]  | [``Poll``](https://docs.rs/futures/0.3.1/futures/task/enum.Poll.html) | This is the representation of the outcome of thinking on a ``Thinkable``. It could either be the actual result or indicating a pending state meaning this need to re-thought by the brain. 
+//! [``Wakeable``]    | [``ArcWake``](https://docs.rs/futures/0.3.1/futures/task/trait.ArcWake.html) | This trait is implemented for the ``Thought`` and indicates that this one could be re-thought by the brain by waking it.
+//! [``Waker``]       | | This is the abstract representation of something that could be waken up. It could be seen as a storage for a ``RawWaker`` that contains the function pointers to the ``Wakeable``s wake functions.
+//! [``Context``]     | | This is actually the container used to pass the the ``Waker`` along to the ``Thinkable`` to allow the same to use the contained ``Waker`` (a clone of it) to be registers for waking up this ``Thinkable`` if it was not yet able to provide a ``Conclusion``
+//! ``spawn``         | | This function is used to spawn new ``Thinkables``s to the ``Brain`` so it could think on those to make progress and try to come to a ``Conclusion``. The ``Thinkable`` is internally wrapped into a ``Thought`` and pushed to the *need-to-think-on-queue*
 //!
 //!
 //!
 
 #[macro_use]
-#[cfg(not(any(test, doctest)))]
 extern crate ruspiro_boot;
 extern crate alloc;
 
@@ -71,7 +75,7 @@ extern "C" {
 fn alive(core: u32) {
     // do the initial setup of the brain as soon as the main core, which is the first, is alive
     if core == 0 {
-        println!("\r\n########## RusPiRo ------------ BRAIN v0.0.1  ------------ ##########");
+        println!("\r\n########## RusPiRo ------------ BRAIN v0.1.0  ------------ ##########");
         BRAIN.take_for(|brain| brain.initialize());
         // if the initialization of the ``Brain`` has been done we could call the entry point the
         // user of this crate will provide. This entry-point returns the initial ``Thought`` the
@@ -79,10 +83,10 @@ fn alive(core: u32) {
         // thought on
         // TODO: until we exactly know how to provide an external entrypoint that could act as the
         // main ``Thought`` just call an awaken function at the consumer side of this crate
-        // This function has to spawn it's ``Thoughts`` to the ``Brain`` if it like to make async
+        // This function has to spawn it's ``Thinkables`` to the ``Brain`` if it like to make
         // progress on them, to ensure all cores are kicked off before doing so, move this into the
         // ``run`` function for the time beeing...
-        // unsafe { awake_with() };
+        unsafe { awake_with() };
     }
 }
 
@@ -91,7 +95,7 @@ fn run(core: u32) -> ! {
         // the main core shall call the crates consumers awaking function to get the initial ``Thought``
         // spawned to the ``Brain``. If this function does not spawn any ``Thought`` the ``Brain``
         // falls asleep
-        unsafe { awake_with() };
+        //unsafe { awake_with() };
     }
 
     // as progress on Thoughts are quite likely depending on interrupt sources, enable them
@@ -129,7 +133,7 @@ fn run(core: u32) -> ! {
 /// /* implementation omitted */
 /// }
 ///
-/// impl Thought for FooThought {
+/// impl Thinkable for FooThought {
 ///     type Output = ();
 ///     fn think(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Conclusion<Self::Output> {
 ///         /* implementation omitted */
@@ -139,8 +143,8 @@ fn run(core: u32) -> ! {
 ///
 ///
 /// fn doc() {
-///     let thought = FooThought{ /* fields omitted */};
-///     spawn(thought);
+///     let thinkable = FooThought{ /* fields omitted */};
+///     spawn(thinkable);
 /// }
 /// ```
 pub fn spawn(thinkable: impl Thinkable<Output = ()> + 'static + Send) {
@@ -185,6 +189,3 @@ macro_rules! awake_with {
         }
     };
 }
-
-#[cfg(test)]
-mod tests;
